989aaa0bd045eb025c938ac2795a5841fac4b3f4
dishabhatt717 <49860464+dishabhatt717@users.noreply.github.com>
2019-05-14 12:53:36 +0530

    Add files via upload

---
+++ login/src/login/Login.java
@@ -0,0 +1,29 @@
+package login;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+
+		if (username.equals("admin") && password.equals("pass123"))
+			out.println("<h2> Login Sucessful</h2>");
+		else
+			out.println("<h2> Login Uncessful</h2>");
+	}
+
+}
